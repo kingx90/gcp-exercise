5d474a9d6b2b3f9c3a47cc9bff410f332e258549
kingsley <kingsleyyakubu810@gmail.com>
2024-11-06 10:36:23 +0100

    ain

--- main.tf
+++ main.tf
@@ -56,3 +56,16 @@ resource "google_compute_subnetwork" "subnet" {
    region        = "asia-northeast1"
    network       = "network-king"
 }
+
+
+provider "google" {
+  project = "project-kingsley-433618"
+  region  = "asia-east1"  
+}
+
+resource "google_storage_bucket" "bucket-king" {
+  name     = "my-unique-bucket-1"
+  location = "Asia" 
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+   }
