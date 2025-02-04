80cdcd8b061542e6119b83eb87afda89b2989faa
unknown <your_email@example.com>
2025-02-04 05:08:18 -0800

    update repository

--- variable.tf
+++ variable.tf
@@ -1,4 +1,69 @@
-resource "google_folder" "folders" {
-  display_name = "e-buisness"
+resource "google_folder" "e-buisness" {
+  display_name = "hub"
+  parent       = "organizations/491563515187"
+}
+
+resource "google_folder" "e-buisness" {
+  display_name = "ops"
+  parent       = "organizations/491563515187"
+}
+
+resource "google_folder" "e-buisness" {
+  display_name = "search"
+  parent       = "organizations/491563515187"
+}
+
+resource "google_folder" "e-buisness" {
+  display_name = "ordermanagement"
+  parent       = "organizations/491563515187"
+}
+
+resource "google_folder" "e-buisness" {
+  display_name = "businessintelligence"
+  parent       = "organizations/491563515187"
+}
+
+resource "google_folder" "e-buisness" {
+  display_name = "supplymanagement"
+  parent       = "organizations/491563515187"
+}
+
+resource "google_folder" "e-buisness" {
+  display_name = "productcreation"
+  parent       = "organizations/491563515187"
+}
+
+resource "google_folder" "e-buisness" {
+  display_name = "integration"
+  parent       = "organizations/491563515187"
+}
+
+resource "google_folder" "e-buisness" {
+  display_name = "omnichannel"
+  parent       = "organizations/491563515187"
+}
+
+resource "google_folder" "e-buisness" {
+  display_name = "consumerapp"
+  parent       = "organizations/491563515187"
+}
+
+resource "google_folder" "e-buisness" {
+  display_name = "customerselfservice"
+  parent       = "organizations/491563515187"
+}
+
+resource "google_folder" "e-buisness" {
+  display_name = "productpresentation"
+  parent       = "organizations/491563515187"
+}
+
+resource "google_folder" "e-buisness" {
+  display_name = "productinformation"
+  parent       = "organizations/491563515187"
+}
+
+resource "google_folder" "e-buisness" {
+  display_name = "dropshipment"
   parent       = "organizations/491563515187"
 }
